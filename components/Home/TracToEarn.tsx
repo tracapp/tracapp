7e1b0f3f57f1fcb2f61c97520121369fc3ee79de
tracapp <dev@trac.run>
2022-05-10 09:43:43 +0800

    first commit

--- components/Home/TracToEarn.tsx
+++ components/Home/TracToEarn.tsx
@@ -15,7 +15,7 @@ export default function TracToEarn() {
           </div>
           <div className='w-full lg:w-5/12'>
             <div className='pb-8 md:pb-16 px-6 md:px-0'>
-              <h2 className='font-black italic text-[28px]  lg:text-[40px]  xl:text-[60px] 2xl:text-[58px] leading-[1.6] text-brand-dark-100 uppercase' ><span className='text-brand-red'>trac</span>-to-earn </h2>
+              <h2 className='font-black italic text-[28px]  lg:text-[40px]  xl:text-[42px] 2xl:text-[58px] leading-[1.6] text-brand-dark-100 uppercase' ><span className='text-brand-red'>trac</span>-to-earn </h2>
               <h1 className='moonhouse-stroke font-moonhouse text-[96px] xl:text-[120px] 2xl:text-[180px] italic font-normal leading-4 lg:pl-5'>EARN</h1>
             </div>
             <p className='hidden lg:block text-xl 2xl:text-[32px] leading-snug  2xl:pr-36 italic text-brand-dark-100'>TRAC offers diverse in-app gamification features such as sneaker levelling, shoe-minting, and sneaker Boosters. Get your NFT sneakers on the marketplace and start your fitness routine and earn TRAC tokens just by walking, jogging, or running outdoors. </p>
